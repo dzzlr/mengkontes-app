f3be0cde514d8eadf3fda406b32e236c41b1cad7
dzzlr <githubdazzler@gmail.com>
2022-07-14 10:13:13 +0700

    Adding -mfsc Competition, Event, Blog

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blogs>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
  */
-class BlogsFactory extends Factory
+class BlogFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/factories/CompetitionFactory.php
+++ database/factories/CompetitionFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Competitions>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Competition>
  */
-class CompetitionsFactory extends Factory
+class CompetitionFactory extends Factory
 {
     /**
      * Define the model's default state.
